828f508d1d7fb2850434041bb2d6204941a466cd
silviamaa <silvia_cok92@hotmail.com>
2015-01-02 13:18:45 +0100

    avance log

--- uaserver.py
+++ uaserver.py
@@ -12,9 +12,10 @@ ip = sys.argv[1]
 puerto = sys.argv[2]
 fichero_audio = sys.argv[3]
 
-if len(sys.argv) != 4:
+if len(sys.argv) != 2:
     sys.exit("Usage: python server.py " + ip + " " + puerto + " " +
              fichero_audio)
+    #meter error en log
 
 
 class ServerHandler(SocketServer.DatagramRequestHandler):
@@ -37,7 +38,7 @@ class ServerHandler(SocketServer.DatagramRequestHandler):
         self.audio = ""
 
     def startElement(self, name, attrs):
-		"""
+        """
         Método que se llama cuando se abre una etiqueta
         """
         if name == 'account':
@@ -59,12 +60,14 @@ class ServerHandler(SocketServer.DatagramRequestHandler):
 
     def handle(self):
         client_ip = str(self.client_address[0])
+        client_port = str(self.client_address[0])
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print line
             line1 = line.split()
-
+            ip = line1[7]
+            port = line[11]
             # Si no hay más líneas salimos del bucle infinito
             if not line1:
                 break
@@ -72,19 +75,27 @@ class ServerHandler(SocketServer.DatagramRequestHandler):
                 self.wfile.write("SIP/2.0 100 Trying\r\n\r\n" +
                                  "SIP/2.0 180 Ringing\r\n\r\n" +
                                  "SIP/2.0 200 OK\r\n\r\n")
+                #contenido SDP
+
+                #meter envio en log            
+
             elif (line1[0] == "ACK"):
                 #enviamos audio
-                reproducir = ('./mp32rtp -i ' + client_ip + ' -p 23032 < ' +
-                              fichero_audio)
+                reproducir = ('./mp32rtp -i ' + ip + ' -p ' +
+                              port + ' < ' + cHandler.audio)
                 print "Listening... ", reproducir
                 os.system(reproducir)
                 print "Se ha terminado de reproducir"
             elif (line1[0] == "BYE"):
                 self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
+                #meter envio en log
+                #meter "finishing" en log
             elif len(line1) != 3:
                 print "SIP/2.0 400 Bad Request"
+                #meter error en log
             else:
                 print "SIP/2.0 405 Method Not Allowed"
+                #meter error en log
 
 if __name__ == "__main__":
     # Creamos servidor y escuchamos
@@ -95,6 +106,7 @@ if __name__ == "__main__":
     parser.setContentHandler(cHandler)
     parser.parse(open(str(sys.argv[1])))
 
-    serv = SocketServer.UDPServer((ip, int(puerto)), EchoHandler)
-    print "Lanzando servidor..."
+    serv = SocketServer.UDPServer((cHandler.uaserver_ip,
+                                   int(cHandler.uaserver_ip)), EchoHandler)
+    print "Listening..."
     serv.serve_forever()
