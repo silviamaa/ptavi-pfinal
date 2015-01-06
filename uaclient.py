c2fff598c67d54c39cd1093db009513324f50425
silviamaa <silvia_cok92@hotmail.com>
2015-01-06 15:03:38 +0100

    casi terminado uaclient

--- uaclient.py
+++ uaclient.py
@@ -8,12 +8,11 @@ import socket
 import sys
 import time
 import os
-
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
-def AñadirLog(evento):
+def MeterLog(evento):
     fichero = open(cHandler.log, 'a')
     mensaje = str(time.time()) + " " + evento + '\r\n'
     fichero.write(mensaje)
@@ -67,15 +66,14 @@ if __name__ == "__main__":
     parser.setContentHandler(cHandler)
     parser.parse(open(str(sys.argv[1])))
 
-
     if len(sys.argv) != 4:
         sys.exit("Usage: python uaclient.py config method option")
-    #meter error en log
-
-    # Contenido que vamos a enviar si es un INVITE o un BYE
-    #MENSAJE = METODO + " sip:" + login + " SIP/2.0\r\n"
+        #meter error en log
+        error = 'Error:  Usage: python uaclient.py config method option'
+        MeterLog(error)
 
     METODO = sys.argv[2]
+    OPCION = SYS.ARGV[3]
     IP = cHandler.regproxy_ip
     PORT = cHandler.regproxy_port
     IP_PORT = str(IP) + ':' + str(PORT)
@@ -85,53 +83,71 @@ if __name__ == "__main__":
     my_socket.connect((IP, int(PORT)))
 
     if (METODO == "REGISTER"):
-		EXPIRES = sys.argv[3]
-        #meter "staring" en log
+        MENSAJE = (METODO + ' sip:' + cHandler.username + ':' +
+                   cHandler.uaserver_port + ' ' + 'SIP/2.0\r\n' + 
+                   'Expires: ' + OPCION)
+        print "Enviando: " + MENSAJE
+        my_socket.send(MENSAJE + '\r\n')
+    elif (METODO == "INVITE"):      
+        SDP = 'Content-Type: application/sdp\r\n' + 'v=0' + '\n'
+        SDP += 'o=' + str(cHandler.username) + ' '
+        SDP += str(cHandler.uaserver_ip)
+        SDP += '\n' + 's=misesion\n' + 't=0\n' + 'm=audio '
+        SDP += str(cHandler.rtp_puerto) + ' RTP'
+        MENSAJE = METODO + ' sip:' + OPCION + ' SIP/2.0\r\n' + SDP
+        print "Enviando: " + MENSAJE
+        my_socket.send(MENSAJE + '\r\n')
+    elif (METODO == "BYE"):
+        MENSAJE = METODO + ' sip:' + OPCION + ' SIP/2.0\r\n'
+        print "Enviando: " + MENSAJE
+        my_socket.send(MENSAJE + '\r\n')    
+    else:       
+        MENSAJE = METODO + 'sip:' + OPCION + ' SIP/2.0\r\n'
+        print "Enviando: " + MENSAJE
+        my_socket.send(MENSAJE + '\r\n')
 
-    if (METODO == "INVITE"):
-		LOGIN = str(sys.argv[3])
-        #print "Enviando: " + MENSAJE
-        #my_socket.send(MENSAJE + '\r\n')
-    if (METODO == "BYE"):
-        #print "Enviando: " + MENSAJE
-        #my_socket.send(MENSAJE + '\r\n')
-
-    #meter envio en log
+    #meter envío en log
+    envio = 'Send to: ' + IP_PORT + ':' + MENSAJE + '\r\n'
+    MeterLog(envio)
 
     #Compruebo que el puerto está abierto
     try:
         data = my_socket.recv(1024)
     except socket.error:
         #meter error en log("Error: No server listening at ")
-
-    #meter recepcion en log   
-	
-	if (data == ("SIP/2.0 200 OK\r\n\r\n")):
-		print 'Recibida respuesta REGISTER-- \r\n\r\n', data
-	
-	data = data.split()
-	server_ip = data[13]
+        error = str(time.time()) + 'Error: no server listening at ' + IP_PORT
+        MeterLog(error)
+        sys.exit()
+    #meter recepcion en log  
+    recepcion = 'Received from ' + IP_PORT + ':' + data + '\r\n'
+    EscribirEnLog(recepcion) 
+    
+    if (data == ("SIP/2.0 200 OK\r\n\r\n")):
+        print 'Recibida respuesta REGISTER-- \r\n\r\n', data
+    
+    data = data.split()
+    server_ip = data[13]
     server_port = data[17]
     elif (data[2] == "Trying" + data[5] == "Ringing" + data[8] == "OK"):
         print 'Recibida respuesta INVITE-- \r\n\r\n', data
         #Se envía el ACK
         mensaje_ack = "ACK" + " sip:" + LOGIN + " SIP/2.0\r\n"
         my_socket.send(mensaje_ack + '\r\n')
-        data = my_socket.recv(1024)
-        print 'Recibida respuesta ACK -- \r\n\r\n', data
-
+        #meter envío en log
+        envio = 'Send to: ' + IP_PORT + ':' + mensaje_ack + '\r\n'
+        MeterLog(envio)
         #enviamos audio(RTP)  
-		reproducir = ('mp32rtp -i ' + server_ip + ' -p ' + puerto_server +
-					  ' < ' + cHandler.audio)
-	    print "Listening... ", reproducir
+        reproducir = ('mp32rtp -i ' + server_ip + ' -p ' + server_port +
+                      ' < ' + cHandler.audio)
+        print "Listening... ", reproducir
         os.system(reproducir)
         print "Se ha terminado de reproducir"     
 
     if (METODO == "BYE"):
-		LOGIN = str(sys.argv[3])
         print 'Recibida respuesta BYE-- \r\n\r\n', data
-
-   		#meter envio en log
+           #meter "finishing" en log
+        despedida = 'finishing...'
+        MeterLog(despedida)
 
     # Cerramos todo
     my_socket.close()
