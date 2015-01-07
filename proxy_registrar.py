0f5f64590af2ad1820cccfc11fb82ba53c8b12fa
silviamaa <silvia_cok92@hotmail.com>
2015-01-07 15:45:44 +0100

    practicamente terminado proxy_registrar.py

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -58,14 +58,14 @@ class Handler(SocketServer.DatagramRequestHandler):
         hora = time.time()
         while 1:
             line = self.rfile.read()
-            line1 = line.split()
-            
+            line1 = line.split() 
             if line1[0] == "REGISTER":
                 line2 = line1[1]
                 line3 = line2.split(":")
                 user = line3[1]
                 dicc_usuario[user] = ip
                 port = int(line3[2])
+                IP_PORT = str(ip) + ':' + str(port)
                 if line1[4] == '0':
                     if user in dicc_usuario:
                         del dicc_usuario[user]
@@ -78,31 +78,59 @@ class Handler(SocketServer.DatagramRequestHandler):
                     dicc_usuario[user] = ip + ',' + str(hora_actualizada)
                     self.register2file()
                     self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
-
-            if line1[0] == "INVITE":
-            #busco si el usuario existe y envío el INVITE, si no, envío 
-            #"404 user not found"
-
-            #meter en log envío y recepcion
-
-            if line1[0] == "ACK":
-            #se retransmite al destino
-
-            #meter en log envio
-            
-            if line1[0] == "BYE":
-            #se retransmite al destino y se envia la respuesta al uaclient
-            
-            #meter en log envio, recepcion y envio
-
+            else:
+                #busco si el usuario existe y envío el mensaje, si no, envío 
+                #"404 user not found"
+                if user in dicc_usuario:
+                    if (line1[0] == "INVITE" or "ACK" or "BYE"):            
+                        self.my_socket.setsockopt(socket.SOL_SOCKET,
+                                                  socket.SO_REUSEADDR, 1)
+                        self.my_socket.connect((ip,port))
+                        self.my_socket.send(line)
+                        print "Enviando: " + line
+                        #meter envío en log
+                        envio = 'Send to: ' + IP_PORT + ':' + line + '\r\n'
+                        MeterLog(envio)
+                        if (line1[0] == "INVITE" or "BYE"):
+                            #Compruebo que el puerto está abierto
+                            try:
+                                data = my_socket.recv(1024)
+                                print 'Recibida respuesta-- \r\n\r\n', data
+                            except socket.error:
+                                #meter error en log("Error: No server listening at")
+                                error = ('Error: no server listening at ' + IP_PORT)
+                                MeterLog(error)
+                                sys.exit()
+                            #meter recepcion en log  
+                            recepcion = ('Received from ' + IP_PORT + ':' + data +
+                                         '\r\n')
+                            EscribirEnLog(recepcion)         
+                            #Envio al user agent el mensaje que recibo
+                            print ("Enviando: " + data)
+                            self.wfile.write(data)
+                            #meter envio en log
+                            envio = 'Send to: ' + IP_PORT + ':' + data + '\r\n'
+                            MeterLog(envio)
+                    else:
+                        print "SIP/2.0 405 Method Not Allowed"
+                        #meter error en log
+                        error = ("Send to " + IP_PORT + ':' +
+                                 "SIP/2.0 405 Method Not Allowed")
+                        MeterLog(error) 
+                else:
+                    print "SIP/2.0 404 user not found"
+                        #meter error en log
+                        error = ("Send to " + IP_PORT + ':' +
+                                 "SIP/2.0 404 user not found")
+                        MeterLog(error) 
             if not line or "[""]":
                 break
+
     """
     Registrar al usuario con dirección + IP + PORT + fecha de registro + Expires
     """
-
     def register2file(self):
-        registered = open("registered.txt", "w")
+        registered = open("registro_usuarios.txt", "w")
         registered.write('User' + "\t" + 'IP' + "\t" + 'Expires' + '\n')
         for user, valor in dicc_usuario.items():
             ip = valor.split(',')[0]
@@ -123,12 +151,15 @@ if __name__ == "__main__":
     parser.parse(open(str(sys.argv[1]))
 
     if len(sys.argv) != 2:
-        print("Usage: python proxy_registrar.py config")
-        
+        print("Usage: python proxy_registrar.py config")       
         #meter error en log
+        error = ('Error: Usage: python proxy_registrar.py config')
+        MeterLog(error)
          
     # Creamos servidor y escuchamos
     serv = SocketServer.UDPServer((str(cHandler.s_ip),
                                    int(cHandler.s_port)), Handler)
     print "Server MiServidorProxy listening at port " + cHandler.s_port + "..."
+    frase="Server MiServidorProxy listening at port " + cHandler.s_port + "..."
+    MeterLog(frase)
     serv.serve_forever()
